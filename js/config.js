90a630e16754a692b141c35f6a889dc0fabd1328
effect082 <your-email@example.com>
2026-06-22 16:00:29 +0900

    Update GAS_URL in config.js to the new deployment URL

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbwjfHiUS9iJGuO8D3Ok37-AG_rm9T9qIang-lY9W3VqQ7i3JTooQm-HyRe9nFVPwX3b/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbxSf4AejmIUuqDiOoPb8lXYOvCymdcgiwFXjf-mslbZpelbithBq6n-pNXox2X40wGb/exec',
   APP_NAME: '강동어울림복지관 출석부',
   VERSION: '1.0.0'
 };
