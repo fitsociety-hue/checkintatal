935db864c41f1238728cd995b54fa96818528089
effect082 <your-email@example.com>
2026-06-22 14:48:20 +0900

    Update GAS_URL to the new robust deployment

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzDZW7mORR03-L1Rs8pVEjRDecbF_lxWijeT4b29McvVtAMCPNoAScl18DJc35idOc-/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwjfHiUS9iJGuO8D3Ok37-AG_rm9T9qIang-lY9W3VqQ7i3JTooQm-HyRe9nFVPwX3b/exec',
   APP_NAME: '강동어울림복지관 출석부',
   VERSION: '1.0.0'
 };
