24e900f86ef8272086b8de5e1fa6cbcef7bb401f
effect082 <your-email@example.com>
2026-06-25 17:10:59 +0900

    chore: update GAS_URL with newest deployment for sheet row deletion

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // Google Apps Script 웹앱 배포 URL
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbxqnXqWruttaFjHTXYQPUfthJpwoV3C8T6-Yv_rhc7ILQ-79OV_tr2vG4Ki6EpowrHj/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwR_zGn1ZkBF32iDv0qNH747UW5LEfMzmh0uFycEpcEvqMRXfNQfrWOCFUXfSoKPyaq/exec',
   APP_NAME: '강동어울림복지관 출석부',
   VERSION: '1.0.0'
 };
