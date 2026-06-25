d46de98baedb49d8e7c406f8e0a32e3e40f3f439
effect082 <your-email@example.com>
2026-06-25 10:52:43 +0900

    Update GAS API URL

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbw_0jtYFX7SPFxfN63Ubj62a-uk5AvZJJOX6c-ZoZEascAcdn8wrgL_bJmlFcS7t4cN/exec',
+  // Google Apps Script 웹앱 배포 URL
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzTIh2ibaKHMpwwdiEemg04U5ERLVhGTExO3Uyh8kx9VLSabkZuq0q-WuJuezOqcKRb/exec',
   APP_NAME: '강동어울림복지관 출석부',
   VERSION: '1.0.0'
 };
