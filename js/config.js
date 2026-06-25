a395283b88ba8173ebc030827f60c6d766512c3c
effect082 <your-email@example.com>
2026-06-25 10:36:46 +0900

    Update GAS URL for member filter fix

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbw605xGQ2gDvs_v1HoBI78c8k6bxtm40wu0qGCQqlqYQoctcC878PW4_KJ45d7SUjOD/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbw_0jtYFX7SPFxfN63Ubj62a-uk5AvZJJOX6c-ZoZEascAcdn8wrgL_bJmlFcS7t4cN/exec',
   APP_NAME: '강동어울림복지관 출석부',
   VERSION: '1.0.0'
 };
