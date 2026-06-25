236ba46dcc16e8e073d87564b0e7346b13a2ff41
effect082 <your-email@example.com>
2026-06-25 10:20:42 +0900

    Update GAS URL to new deployment

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbx7vMIJVUndOOkBjRbupfGo9ScS5YY4hhw6xc-d13x5lFhy2VCJZloT3zDsu9Qm5hWS/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbw605xGQ2gDvs_v1HoBI78c8k6bxtm40wu0qGCQqlqYQoctcC878PW4_KJ45d7SUjOD/exec',
   APP_NAME: '강동어울림복지관 출석부',
   VERSION: '1.0.0'
 };
