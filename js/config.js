a2b0c0ffc2efc2c530a1ed09a41a6f865e2773bd
effect082 <your-email@example.com>
2026-06-25 15:42:51 +0900

    chore: update GAS deployment URL for role bugfix

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // Google Apps Script 웹앱 배포 URL
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbxkigHIynLgbMD8GT0wWKfYpfEtE9csVBtKOXiMpRIJAY8RMRxRh-Ado1SzPCZ40ktx/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzq06lqCmkX9aWLKZFezrkTTxei9fnpUTtQCDhdylrEW_CmCz3b_Oqa-uTmAi17VQWd/exec',
   APP_NAME: '강동어울림복지관 출석부',
   VERSION: '1.0.0'
 };
