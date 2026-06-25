358b6573dc13dac1aef9c14b85c5594e90a9c934
effect082 <your-email@example.com>
2026-06-25 15:33:07 +0900

    chore: update GAS deployment URL

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // Google Apps Script 웹앱 배포 URL
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzTIh2ibaKHMpwwdiEemg04U5ERLVhGTExO3Uyh8kx9VLSabkZuq0q-WuJuezOqcKRb/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbxkigHIynLgbMD8GT0wWKfYpfEtE9csVBtKOXiMpRIJAY8RMRxRh-Ado1SzPCZ40ktx/exec',
   APP_NAME: '강동어울림복지관 출석부',
   VERSION: '1.0.0'
 };
