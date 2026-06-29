552d63c653ff487b3da71cc9007c32818ae77351
effect082 <your-email@example.com>
2026-06-29 15:20:26 +0900

    chore: 구글 앱스 스크립트 배포 URL 업데이트

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // Google Apps Script 웹앱 배포 URL
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbwRrPz_Q6fG1trg4JQRQGX1t59OJL6BUe0ejEt9wUoXMn8U72KyUS_vqoS7dT1eQoAR/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzGaHIu4x5-nkqq9ioJmngvqzH0rcFKYHP7Gju96jsCQL2i_PSfZY_yqxhWcPzReEaV/exec',
   APP_NAME: '강동어울림복지관 출석부',
   VERSION: '1.0.0'
 };
